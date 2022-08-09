4eb43e8f91fbbe722779c1b7bd932fe3e4462897
Martin Affolter <martin@affolter.net>
2022-08-09 21:59:53 +0200

    initial commit

---
+++ src/InputText.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+input(type="text")
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/InputTextarea.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+textarea
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss">
+</style>
