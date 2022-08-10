6ec3d1ea8c4315c1fa648f3dbf3cf0d34bb57bc7
Martin Affolter <martin@affolter.net>
2022-08-10 10:17:26 +0200

    export correct types and modules...

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
+import { resolve } from 'path'
+import { existsSync, readdirSync, lstatSync, rmdirSync, unlinkSync } from 'fs'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
+emptyDir(resolve(__dirname, 'types'))
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    // alias: [{ find: /^@\/(.+)/, replacement: resolve(__dirname, '$1') }]
+    alias: {
+      '@': resolve(__dirname),
+      '@components': resolve(__dirname, 'src/components')
+    }
+  },
   plugins: [vue(), dts()],
   build: {
     lib: {
-      entry: 'src/index.ts',
-      name: 'affolternet-vue3-library',
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'affolternetVue3Library',
       fileName: 'affolternet-vue3-library'
     },
     rollupOptions: {
@@ -25,3 +36,21 @@ export default defineConfig({
     },
   },
 })
+
+function emptyDir(dir: string): void {
+  if (!existsSync(dir)) {
+    return
+  }
+
+  for (const file of readdirSync(dir)) {
+    const abs = resolve(dir, file)
+
+    // baseline is Node 12 so can't use rmSync
+    if (lstatSync(abs).isDirectory()) {
+      emptyDir(abs)
+      rmdirSync(abs)
+    } else {
+      unlinkSync(abs)
+    }
+  }
+}
